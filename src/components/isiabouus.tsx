b2fb6e44990bf2437b4613a14847086136eb8ea6
abidmul <mulawaabid@gmail.com>
2024-03-22 10:45:19 +0700

    website up to date

--- src/components/isiabouus.tsx
+++ src/components/isiabouus.tsx
@@ -8,7 +8,7 @@ export default function IsiAboutUs() {
       <div className="carousel w-full h-72">
         <div id="slide1" className="carousel-item relative w-full">
           <Image
-            src="https://images.unsplash.com/photo-1610912591861-4ea5fae8f804?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1aWxkaW5nJTIwb2ZmaWNlfGVufDB8fDB8fHww" loading="lazy" width={500} height={303} alt="gambar1"
+            src="https://images.unsplash.com/photo-1610912591861-4ea5fae8f804?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1aWxkaW5nJTIwb2ZmaWNlfGVufDB8fDB8fHww"   width={500} height={303} priority alt="gambar1"
             className="w-full"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -36,7 +36,7 @@ export default function IsiAboutUs() {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <Image
-            src="https://media.istockphoto.com/id/1323649948/photo/shot-of-creatives-working-in-cubicles.webp?b=1&s=170667a&w=0&k=20&c=lTyBpURkXhG0ygX22g5DHyoyRRRu31LnkLdXuBZOu60=" loading="lazy" width={533} height={323} alt="gambar3"
+            src="https://media.istockphoto.com/id/1323649948/photo/shot-of-creatives-working-in-cubicles.webp?b=1&s=170667a&w=0&k=20&c=lTyBpURkXhG0ygX22g5DHyoyRRRu31LnkLdXuBZOu60=" loading="lazy"  width={533} height={323} alt="gambar3"
             className="w-full"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -50,7 +50,7 @@ export default function IsiAboutUs() {
         </div>
         <div id="slide4" className="carousel-item relative w-full">
           <Image
-            src="https://images.unsplash.com/photo-1552664730-d307ca884978?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTN8fGNhcm91c2VsJTIwb2ZmaWNlfGVufDB8fDB8fHww" loading="lazy" width={500} height={333} alt="gambar4"
+            src="https://images.unsplash.com/photo-1552664730-d307ca884978?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTN8fGNhcm91c2VsJTIwb2ZmaWNlfGVufDB8fDB8fHww"loading="lazy" width={500} height={333} alt="gambar4"
             className="w-full"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
